f5438e1b5451525f9b9e9ab93153e91196ab9ecb
Udalov-Kirill <oracle-git@mail.ru>
2023-04-23 14:00:38 +0700

    Div

--- Laba3/Laba3/Program.cs
+++ Laba3/Laba3/Program.cs
@@ -6,5 +6,16 @@
         {
 
         }
+        static void Div(double number1, double number2)
+        {
+            double div = 0;
+            if (number2 != 0)
+            {
+                div = number1 / number2;
+                Console.WriteLine("Частное чисел = " + div);
+            }
+            else
+                Console.WriteLine("Поменяйте значение числа В");
+        }
     }
 }
